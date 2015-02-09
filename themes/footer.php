c2b6fb5e2a89f88055f4e196a4e8686659db60fb
Jorge Pantoja <jorgepantojam@gmail.com>
2015-02-09 11:56:05 -0500

    Archivos Iniciales

---
+++ themes/footer.php
@@ -0,0 +1,44 @@
+
+<footer id="innovacion_footer_page">
+	
+	<div class="footer_1">
+		<div class="separador_vertical_4"></div>
+		
+		<div class="row">
+		  <br>
+		  <div class="col-sm-6 col-xs-12 footer_custom_padding_left_1 footer_snap_logo_custom_top"><img src="<?= $wp_path_innovacion ?>img/logos/snap.png"></div>
+		  <div class="col-sm-6 col-xs-12 footer_custom_padding_right_1">
+		  	<div class="footer_text">
+				García Moreno N10-43 Entre Chile y Espejo<br>
+				Código Postal: 170401 - Quito - Ecuador<br>
+				Horario de Atención: Lunes - Viernes 8h00 a 17h00<br>
+				Teléfono: 593-2 258-0716
+	  		</div>
+		  </div>
+		</div>
+		
+	</div>
+	
+</footer>
+
+<?php include_once('end_scripts.php'); ?>
+
+<!-- Comments Count Disqus -->
+
+<script type="text/javascript">
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'plataformainnovacion'; // required: replace example with your forum shortname
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function () {
+var s = document.createElement('script'); s.async = true;
+s.type = 'text/javascript';
+s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
+</script>
+
+<!-- End Comments Count Disqus -->
+
+</body>
+</html>
---
+++ themes/header_2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="es">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <title><?php echo $title; ?></title>
+    <meta name="description" content="<?php echo $metaDescription; ?>">
+    <meta name="keywords" content="<?php echo $metaKeywords; ?>" />
+    
+    <link rel="stylesheet" href="<?= $wp_path_innovacion ?>vendor/bootstrap-3.2.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= $wp_path_innovacion ?>vendor/bootstrap-3.2.0-dist/css/bootstrap-theme.css">
+    
+    <link rel="stylesheet" href="<?= $wp_path_innovacion ?>css/main-mobile.css">
+    
+    <link href="<?= $wp_path_innovacion ?>vendor/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    
+    <script src="<?= $wp_path_innovacion ?>vendor/jquery-1.11.1.min.js" type="text/javascript"></script>
+    <script src="<?= $wp_path_innovacion ?>vendor/bootstrap-3.2.0-dist/js/bootstrap.js" type="text/javascript"></script>
+    <script src="<?= $wp_path_innovacion ?>vendor/modernizr-2.8.0.min.js" type="text/javascript"></script>
+    <script src="<?= $wp_path_innovacion ?>js/main.js" type="text/javascript"></script>
+    
+    <!--[if IE 8]>
+        <link rel="stylesheet" href="css/main_ie.css">
+    <![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<!--[if lt IE 7]>  <body class="ie ie6 lte9 lte8 lte7">  <![endif]-->
+<!--[if IE 7]>     <body class="ie ie7 lte9 lte8 lte7">  <![endif]-->
+<!--[if IE 8]>     <body class="ie ie8 lte9 lte8">       <![endif]-->
+<!--[if IE 9]>     <body class="ie ie9 lte9">            <![endif]-->
+<!--[if gt IE 9]>  <body class="ie">                     <![endif]-->
+<!--[if !IE]><!--> <body>                               <!--<![endif]-->    
+    
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
