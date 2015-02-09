30d98e7eb396ef6c81f47740c2e44cb5b33d9a38
Jorge Pantoja <jorgepantojam@gmail.com>
2015-02-09 12:53:28 -0500

    Cambios

---
+++ revista/wp-content/themes/flex/templates/header/slogan.php
@@ -0,0 +1,2 @@
+<?php global $theme_options; ?>
+<div class="header-slogan">Contacto: <i class="icon-phone"></i><a href="#">+(593 2) 3934-500 </a> or <i class="icon-envelope-alt"></i> <a href="#">direccion.innovacion@administracionpublica.gob.ec</a></div>
