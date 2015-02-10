890b39047e1f4e13ece80d3df0dd42c12cd8cc9d
Jorge Pantoja <jorgepantojam@gmail.com>
2015-02-10 13:19:42 -0500

    subir

--- header_wp_temp.php
+++ header_wp_temp.php
@@ -1,12 +1,17 @@
 <?php 
 
-define('WP_USE_THEMES', true);
-require('http://innovacionlab.gob.ec/revista/wp-load.php'); 
+define('WP_USE_THEMES', false);
+include('revista/wp-load.php'); 
+
+$recent_posts = wp_get_recent_posts(array(
+	'numberposts' => 10
+));
+	
+// Do something with them
+echo '<ul>';
+foreach($recent_posts as $post) {
+	echo '<li><a href="', get_permalink($post['ID']), '">', $post['post_title'], '</a></li>';
+}
+echo '</ul>';
 
 ?>
-
-<?php get_header(); ?>
-
-Prueba
-
-<?php get_footer(); ?>
