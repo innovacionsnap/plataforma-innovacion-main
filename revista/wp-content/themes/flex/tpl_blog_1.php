ab04327ea9a646599a8892acd1590d4c9de0973e
Jorge Pantoja <jorgepantojam@gmail.com>
2015-02-24 11:41:52 -0500

    cambios

--- revista/wp-content/themes/flex/tpl_blog_1.php
+++ revista/wp-content/themes/flex/tpl_blog_1.php
@@ -17,29 +17,6 @@ Template Name: Blog Posts SNAP
 		query_posts('post_type=post&post_status=publish&category_name='.$category_name.'&posts_per_page=10&paged='. get_query_var('paged')); 
 		
 		?>
-		
-		<?php if (is_author() || is_archive() || is_day() || is_tag() || is_category() || is_month() || is_day() || is_year()): ?>
-
-			<h3 class="search-title">
-				<?php if (is_category()): ?>
-					<?php echo __("Category: ", MD_THEME_NAME);  echo '<span>'.single_cat_title("", false).'</span>'; ?>
-				<?php elseif(is_tag()): ?>
-					<?php echo __("Tag: ", MD_THEME_NAME);  echo '<span>'.single_tag_title("", false).'</span>'; ?>
-				<?php elseif(is_day()): ?>
-					<?php echo __("Day: ", MD_THEME_NAME);  echo '<span>'.get_the_time('F jS, Y').'</span>'; ?>
-				<?php elseif(is_month()): ?>
-					<?php echo __("Month: ", MD_THEME_NAME);  echo '<span>'.get_the_time('F, Y').'</span>'; ?>
-				<?php elseif(is_year()): ?>
-					<?php echo __("Year: ", MD_THEME_NAME);  echo '<span>'.get_the_time('Y').'</span>'; ?>
-				<?php elseif(is_author()): ?>
-					<?php global $author; $userdata = get_userdata($author); ?>
-					<?php echo __("Author:", MD_THEME_NAME); ?> <?php echo '<span>'.$userdata->display_name.'</span>'; ?>
-				<?php else: ?>
-					<?php echo __("Archive", MD_THEME_NAME); ?>
-				<?php endif ?>
-			</h3>
-				
-		<?php endif; ?>
 	
 		<?php get_template_part('/templates/blog/blog-layout');?>	
 
