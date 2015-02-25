ee136e3fcbdaa80f565a9325330b67924cec6839
Jorge Pantoja <jorgepantojam@gmail.com>
2015-02-24 20:03:42 -0500

    cambios

--- revista/wp-content/themes/flex/home.php
+++ revista/wp-content/themes/flex/home.php
@@ -5,14 +5,6 @@ $bg_home_posts = $theme_options['body-bgcolor'];
 get_header(); 
 
 ?>
-
-	<?php
-		
-		if(get_post_field('post_content', get_option('page_for_posts'))){
-			echo apply_filters('the_content', get_post_field('post_content', get_option('page_for_posts')));
-		}
-		
-	?>
 	
 	<div class="page-content" id="blog-container">
 		<div class="container">
